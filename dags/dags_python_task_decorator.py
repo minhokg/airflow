75823124fc589e66c85d72ff4fd384e952624c35
minhokg <rkdalsgh32@gmail.com>
2025-08-05 11:58:50 +0200

    Upload dags_python_task_decorator again

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,19 @@
+from airflow.decorators import dag
+from airflow.decorators import task
+import pendulum
+
+@dag(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+)
+@task(task_id="python_task_1")
+def print_context(ds=None, **kwargs):
+    """Print the Airflow context and ds variable from the context."""
+    pprint(kwargs)
+    print(ds)
+    return "Whatever you return gets printed in the logs"
+g
+python_task_1 = print_context(ds='Run task decorator')
