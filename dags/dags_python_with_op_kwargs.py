d71be2f6123bf5875b4fdf0df119e8253ce7a3a1
minhokg <rkdalsgh32@gmail.com>
2025-08-05 16:15:42 +0200

    Upload dags_python_with_op_kwrags.py

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import dag
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz='UTC'),
+    catchup=False
+) as dag:
+    
+    regist = PythonOperator(
+        task_id = "regist",
+        python_callable=regist,
+        op_args=['minhokg','man','kr','seoul'],
+        op_kwargs={'email':'minho.kg97@gmail.com','phone':'015754719674'},
+        )
+
+    regist
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,13 @@
 def get_sftp():
     print("Get sftp")
+
+def regist(name, sex, *args, **kwargs):
+    print(f'Name: {name}')
+    print(f'Sex: {sex}')
+    print(f'Other options: {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
