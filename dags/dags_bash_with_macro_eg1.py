359a851328dbe9cec8fecbba6dddb35e77d89760
minhokg <rkdalsgh32@gmail.com>
2025-08-06 16:55:47 +0200

    Create dags_bash_with_macro_eg

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz='UTC'),
+    catchup=False
+) as dag:
+    bash_task_1 = BashOperator(
+        task_id = "bash_task_1",
+        env={'START_DATE':'{{ data_interval_start.in_timezone("UTC") | ds}}',
+             'END_DATE': '{{ data_interval_end.in_timezone("UTC") - macros.dateutil.relativedelta.relativedelta(days=-1) | ds}}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE : $END_DATE"'
+    ),
+    
+    
